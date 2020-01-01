a85ee8a26354604f2946c6e5053537f31becd520
Mark Mauro <markymauro@gmail.com>
2020-01-01 17:25:11 -0500

    Update my_twitter_bot.py

--- files/my_twitter_bot.py
+++ files/my_twitter_bot.py
@@ -3,10 +3,10 @@ import time
 
 print("this is my twitter bot")
 
-CONSUMER_KEY = 'kwbh7wUyLFhsmZ9WYzahXXRly'
-CONSUMER_SECRET = 'Mt1N8cSVUla6YKw5D3bg5cNymXatigZMwSDObKDfCWafTQg7EY'
-ACCESS_KEY = '3045426691-sm1ENWdt14Mr989tD7MJX3iz5AkNgpdLnliguWS'
-ACCESS_SECRET = 'vrBRatAVVgnl9YcOZ6vJbtGd59pjmWfShDAyX2tUbBsZ5'
+CONSUMER_KEY = 'x'
+CONSUMER_SECRET = 'x'
+ACCESS_KEY = 'x-x'
+ACCESS_SECRET = 'x'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
