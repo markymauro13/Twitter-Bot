308bc0d3c6f4acecb7d24b070b6d97ec44004163
Mark Mauro <markymauro@gmail.com>
2020-09-11 16:44:39 -0400

    commenting out a line

--- files/tweet_from.py
+++ files/tweet_from.py
@@ -15,4 +15,4 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status("tweet")
+# api.update_status("tweet")
